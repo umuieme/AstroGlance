46048fbe9f4fccb440a9ef39662dc6c9fc2a8aa4
Umesh Basnet <basnet0umesh@gmail.com>
2025-06-08 14:47:01 -0400

    Initial Commit

---
+++ AstroGlance/AstroGlanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroGlanceApp.swift
+//  AstroGlance
+//
+//  Created by Umesh Basnet on 2025-06-08.
+//
+
+import SwiftUI
+
+@main
+struct AstroGlanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
